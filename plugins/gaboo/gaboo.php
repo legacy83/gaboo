9abea2c5f55e694b60b3f18d30fbc79e280fe674
Thiago Senna <trsenna@gmail.com>
2015-08-13 21:24:26 -0300

    saving progress

--- plugins/gaboo/gaboo.php
+++ plugins/gaboo/gaboo.php
@@ -14,14 +14,27 @@
  * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
+add_action( 'gaboo_back_compat_safe', 'gaboo_safe_includes', 0 );
+add_action( 'gaboo_back_compat_safe', 'gaboo_safe_bootstrap', 0 );
+
+/**
+ * Safely continues
+ * ... with the includes needed by the plugin
+ */
+function gaboo_safe_includes()
+{
+    require_once( 'includes/class-gaboo-clip.php' );
+    require_once( 'includes/class-gaboo-lock.php' );
+}
+
 /*
  * Turn on the lights
  * ... and bootstrap the plugin
  */
 
-require_once( 'includes/gaboo-bootstrap.php' );
-require_once( 'includes/class-gaboo-activated-last.php' );
-require_once( 'includes/class-gaboo-back-compat.php' );
+require_once( 'base/class-gaboo-activated-last.php' );
+require_once( 'base/class-gaboo-back-compat.php' );
+require_once( 'base/gaboo-bootstrap.php' );
 
 __gaboo_run( new Gaboo_Back_Compat() );
 __gaboo_plugins_loaded( new Gaboo_Activated_Last( __FILE__ ) );
---
+++ plugins/gaboo/includes/class-gaboo-lock.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Class Gaboo_Lock
+ *
+ */
+final class Gaboo_Lock
+{
+    function __run()
+    {
+        $this->disallow_file_changes();
+    }
+
+    /**
+     * Safely disallow accidental file changes.
+     */
+    function disallow_file_changes()
+    {
+        defined( 'DISALLOW_FILE_EDIT' ) or define( 'DISALLOW_FILE_EDIT', TRUE );
+        defined( 'DISALLOW_FILE_MODS' ) or define( 'DISALLOW_FILE_MODS', TRUE );
+    }
+
+}
